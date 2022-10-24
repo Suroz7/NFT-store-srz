9d3222bc45f4fa14dcd0b8881e4fe102f0a4fc38
Suroz7 <48718555+Suroz7@users.noreply.github.com>
2022-10-24 11:27:14 +0300

    Revert "first commit"

--- js/packages/web/src/views/home/components/SalesList/index.tsx
+++ js/packages/web/src/views/home/components/SalesList/index.tsx
@@ -38,8 +38,8 @@ export const SalesListView = () => {
     <>
       <Banner
         src="/main-banner.svg"
-        headingText="The amazing world of NFTS."
-        subHeadingText="Buy exclusive NFTs DEsigned by Suroz."
+        headingText="The amazing world of Metaplex."
+        subHeadingText="Buy exclusive Metaplex NFTs."
         actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
         useBannerBg
       />
